670f02b36082aaa9d298ae9ac2334df645e4ae3e
zyb <zybbyzaa@163.com>
2016-09-30 17:02:22 +0800

    fix minify

--- workflow/tasks/TaskCompileJs.js
+++ workflow/tasks/TaskCompileJs.js
@@ -6,9 +6,10 @@ module.exports = function (gulp, common) {
     gulp.task('compile_js', function() {
         common.plugins.util.log('开始编译js');
         return gulp.src(common.config.paths.src.js)
+            .pipe(common.plugins.if(argv.env == 'prod',gulp.dest(common.config.paths.dist.js),gulp.dest(common.config.paths.dev.js)))
             .pipe(common.plugins.if(argv.env == 'prod',common.plugins.uglify()))
             .pipe(common.plugins.if(argv.env == 'prod',common.plugins.rename({ suffix: '.min' })))
-            .pipe(common.plugins.if(argv.env == 'prod',gulp.dest(common.config.paths.dist.js),gulp.dest(common.config.paths.dev.js)))
+            .pipe(common.plugins.if(argv.env == 'prod',gulp.dest(common.config.paths.dev.js)))
             .on('end',function(){
                 lib.task_log('compile_js');
             });
