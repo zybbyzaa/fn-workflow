895a9fd2beaebcb51124a8b30d1626bae159cb36
zyb <zybbyzaa@163.com>
2017-12-05 10:29:21 +0800

    update task

--- workflow/tasks/TaskRev.js
+++ workflow/tasks/TaskRev.js
@@ -4,33 +4,53 @@ var lib = require('../util/lib');
 
 module.exports = function(gulp, common) {
   gulp.task('rev', function(cb) {
-    var pcStream = gulp
-      .src([
-        `${common.config.paths.src.root}${common.config.paths.src.revSrc}`,
-        `${common.config.paths.dist.root}/WEB-INF/*/*.shtml`,
-        `!${common.config.paths.dist.root}/WEB-INF/common/*.shtml`
-      ])
-      .pipe(
-        common.plugins.revCollector({
-          replaceReved: true
-        })
-      )
-      .pipe(gulp.dest(`${common.config.paths.dist.root}/WEB-INF`));
-    var mobileStream = gulp
-      .src([
-        `${common.config.paths.src.root}${common.config.paths.src
-          .revSrcMobile}`,
-        `${common.config.paths.dist.root}/WEB-INF/m/*/*.shtml`,
-        `!${common.config.paths.dist.root}/WEB-INF/m/common/*.shtml`
-      ])
-      .pipe(
-        common.plugins.revCollector({
-          replaceReved: true
-        })
-      )
-      .pipe(gulp.dest(`${common.config.paths.dist.root}/WEB-INF/m`));
-    return merge2(pcStream, mobileStream).on('end', () => {
-      lib.task_log('rev');
+    var srcPath = [
+      `${common.config.paths.src.root}${common.config.paths.src.revSrc}`,
+      `${common.config.paths.dist.root}/WEB-INF/**/*.shtml`
+    ];
+    var destPath = `${common.config.paths.dist.root}/WEB-INF`;
+    var commonFilter = common.plugins.filter(file => {
+      return !/(\\m)?\\common\\/.test(file.path);
     });
+    return gulp
+      .src(srcPath)
+      .pipe(commonFilter)
+      .pipe(
+        common.plugins.revCollector({
+          replaceReved: true
+        })
+      )
+      .pipe(gulp.dest(destPath))
+      .on('end', () => {
+        lib.task_log('rev');
+      });
+    // var pcStream = gulp
+    //   .src([
+    //     `${common.config.paths.src.root}${common.config.paths.src.revSrc}`,
+    //     `${common.config.paths.dist.root}/WEB-INF/*/*.shtml`,
+    //     `!${common.config.paths.dist.root}/WEB-INF/common/*.shtml`
+    //   ])
+    //   .pipe(
+    //     common.plugins.revCollector({
+    //       replaceReved: true
+    //     })
+    //   )
+    //   .pipe(gulp.dest(`${common.config.paths.dist.root}/WEB-INF`));
+    // var mobileStream = gulp
+    //   .src([
+    //     `${common.config.paths.src.root}${common.config.paths.src
+    //       .revSrcMobile}`,
+    //     `${common.config.paths.dist.root}/WEB-INF/m/*/*.shtml`,
+    //     `!${common.config.paths.dist.root}/WEB-INF/m/common/*.shtml`
+    //   ])
+    //   .pipe(
+    //     common.plugins.revCollector({
+    //       replaceReved: true
+    //     })
+    //   )
+    //   .pipe(gulp.dest(`${common.config.paths.dist.root}/WEB-INF/m`));
+    // return merge2(pcStream, mobileStream).on('end', () => {
+    //   lib.task_log('rev');
+    // });
   });
 };
