608599704694abc76b125fb840dae7c069620bc0
zyb <zybbyzaa@163.com>
2017-12-07 14:56:57 +0800

    refactor(config): 区分默认配置文件与用户配置文件

--- workflow/util/common.js
+++ workflow/util/common.js
@@ -1,22 +1,35 @@
 // 声明插件以及配置文件的依赖
+var fs = require('fs');
 var plugins = require('gulp-load-plugins')(),
-    packageInfo = require('../../package.json'),
-    browserSync = require('browser-sync').create(),
-    reload = browserSync.reload,
-    config;
+  packageInfo = require('../../package.json'),
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload,
+  config,
+  localConfig = {};
 
-// 读取项目配置表
+// 读取项目默认配置
 try {
-    config = require('../../config.js');
+  config = require('../../config/default.js');
 } catch (_event) {
-    plugins.util.log(plugins.util.colors.red('Gulp Config Error: ') + '找不到项目配置文件');
+  plugins.util.log(
+    plugins.util.colors.red('Gulp Config Error: ') + '找不到项目配置文件'
+  );
+}
+
+// 读取项目本地配置
+try {
+  localConfig = require('../../config/local.js');
+} catch (_event) {
+  plugins.util.log(
+    plugins.util.colors.red('Gulp Config Error: ') + '找不到项目配置文件'
+  );
 }
 
 // 创建 common 对象
 var common = {};
 
 common.plugins = plugins;
-common.config = config;
+common.config = Object.assign(config, localConfig);
 common.packageInfo = packageInfo;
 common.browserSync = browserSync;
 common.reload = reload;
